0b526b5878bf4e2045971de54ff479a4d376ed7b
iddrisabdulwahab <79385555+iddrismystic@users.noreply.github.com>
2021-12-26 22:50:35 +0000

    Update fun.js

--- js/fun.js
+++ js/fun.js
@@ -4,3 +4,18 @@ const sidebar = document.querySelector('.sidebar');
 const closeSidebar = document.querySelector('.close-sidebar');
 sidebarTrigger.addEventListener('click', ()=> sidebar.style.width = "250px")
 closeSidebar.addEventListener('click', ()=> sidebar.style.width = "0px")
+//accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
